be330eaa80f97f83b2fa2d364d967c9ccf298986
Enes Çelik <enescelik6012@gmail.com>
2024-06-06 19:56:25 +0300

    feat: Add modal component to shared module
    The code changes add a new modal component to the shared module. The modal component is used in the products component to display a modal for adding a new product. This improves code organization and reusability.
    
    Note: The commit message should be modified to adhere to the repository's commit message conventions.

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+})
+export class ModalComponent {
+  @Input() isVisible: boolean = false;
+  @Input() customFooter = false;
+  @Input() handleOk: () => void = () => {};
+  @Input() handleCancel: () => void = () => {};
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from './button/button.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { ModalComponent } from './modal/modal.component';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @NgModule({
-  declarations: [ButtonComponent],
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, NzButtonModule],
-  exports: [ButtonComponent],
+  declarations: [ButtonComponent, ModalComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NzButtonModule,
+    NzModalModule,
+  ],
+  exports: [ButtonComponent, ModalComponent],
 })
 export class SharedModule {}
